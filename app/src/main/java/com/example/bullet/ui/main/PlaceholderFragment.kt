a3249c4e733a056e7c5538494a8f5a6b70d822a2
Nikita <neketapostolov@gmail.com>
2020-08-02 23:27:15 +0300

    some fixes and add firebase

--- app/src/main/java/com/example/bullet/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/example/bullet/ui/main/PlaceholderFragment.kt
@@ -1,14 +1,7 @@
 package com.example.bullet.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.bullet.R
 
 /**
  * A placeholder fragment containing a simple view.
