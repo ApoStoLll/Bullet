dcab4ebb29bdfe3158e86f6f5969153d6f2078e9
Nikita <neketapostolov@gmail.com>
2020-08-12 00:23:08 +0300

    try to firebaseRecyclerAdapter

--- app/src/main/java/com/example/bullet/ui/main/orderList/OrderListFragment.kt
+++ app/src/main/java/com/example/bullet/ui/main/orderList/OrderListFragment.kt
@@ -1,26 +1,37 @@
 package com.example.bullet.ui.main.orderList
 
+import android.graphics.ColorSpace.Model
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.bullet.R
 import com.example.bullet.adapters.OrderClickHandler
 import com.example.bullet.adapters.OrderListAdapter
 import com.example.bullet.domain.models.Order
 import com.example.bullet.helpers.OrderListState
+import com.firebase.ui.database.FirebaseListOptions
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.firebase.ui.database.SnapshotParser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
 import kotlinx.android.synthetic.main.fragment_order_list.*
 
 
 class OrderListFragment : Fragment() {
 
     private lateinit var viewModel: OrderListViewModel
-    private lateinit var mAdapter: OrderListAdapter
+    private lateinit var adapter: FirebaseRecyclerAdapter<Order, ViewHolder>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,13 +39,13 @@ class OrderListFragment : Fragment() {
     ): View? {
         viewModel = ViewModelProviders.of(this).get(OrderListViewModel::class.java)
         // Inflate the layout for this fragment
-        mAdapter = OrderListAdapter()
+        adapter = OrderListAdapter()
         val orderList = listOf(
             Order(1,"seseg","ege","segeg",52,"seesfe",56),
             Order(1,"seseg","ege","segeg",52,"seesfe",56)
         )
-        mAdapter.setData(orderList)
-        mAdapter.attachClickHandler(object : OrderClickHandler {
+        adapter.setData(orderList)
+        adapter.attachClickHandler(object : OrderClickHandler {
             override fun onItemClick(item: Int) {
                 val bundle = Bundle()
                 bundle.putInt("number",item)
@@ -67,8 +78,54 @@ class OrderListFragment : Fragment() {
         })
         Log.e("tab","works2")
         RecycleOrders.layoutManager = GridLayoutManager(context,1) // Grid
-        RecycleOrders.adapter = mAdapter
+        RecycleOrders.adapter = adapter
         RecycleOrders.recycledViewPool.setMaxRecycledViews(0, 0)
+    }
+
+    private fun fetch() {
+        val query: Query = FirebaseDatabase.getInstance()
+            .reference
+            .child("orders")
+        val options = FirebaseRecyclerOptions.Builder<Order>()
+            .setQuery(query
+            ) { snapshot ->
+                Order(
+                    snapshot.child("id").value.toString().toInt(),
+                    snapshot.child("title").value.toString(),
+                    snapshot.child("from").value.toString(),
+                    snapshot.child("to").value.toString(),
+                    snapshot.child("customerId").value.toString().toInt(),
+                    snapshot.child("description").value.toString(),
+                    snapshot.child("orderPrice").value.toString().toInt()
+                )
+            }
+            .build()
+
+        adapter = object : FirebaseRecyclerAdapter<Order, ViewHolder>(options) {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): ViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.order, parent, false)
+                return object : ViewHolder(view)
+            }
+
+            override fun onBindViewHolder(
+                holder: ViewHolder,
+                position: Int,
+                model: Order
+            ) {
+                holder.setTxtTitle(model.getmTitle())
+                holder.setTxtDesc(model.getmDesc())
+                holder.root.setOnClickListener(View.OnClickListener {
+                    Toast.makeText(this@MainActivity, position.toString(), Toast.LENGTH_SHORT)
+                        .show()
+                })
+            }
+
+        }
+        recyclerView.setAdapter(adapter)
     }
 
 
