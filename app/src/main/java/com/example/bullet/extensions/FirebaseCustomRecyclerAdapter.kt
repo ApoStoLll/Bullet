1fc7b390490d88546a5727830bb7a5eeae36abc6
danil.vladyka1 <danil.vladyka1@gmail.com>
2020-09-04 20:05:22 +0300

    abstract class

--- app/src/main/java/com/example/bullet/extensions/FirebaseCustomRecyclerAdapter.kt
+++ app/src/main/java/com/example/bullet/extensions/FirebaseCustomRecyclerAdapter.kt
@@ -2,10 +2,13 @@ package com.example.bullet.extensions
 
 import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bullet.domain.models.Destination
 import com.example.bullet.domain.models.Order
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.firebase.ui.database.ObservableSnapshotArray
+import com.firebase.ui.database.SnapshotParser
+import com.google.android.gms.maps.model.LatLng
 
 abstract class FirebaseCustomRecyclerAdapter<T : Order, VH : RecyclerView.ViewHolder>(options: FirebaseRecyclerOptions<T>) :
     FirebaseRecyclerAdapter<T, VH>(options) {
@@ -15,8 +18,10 @@ abstract class FirebaseCustomRecyclerAdapter<T : Order, VH : RecyclerView.ViewHo
 
     override fun getSnapshots(): ObservableSnapshotArray<T> {
         val snapshots = super.getSnapshots()
-        snapshots.sortBy { it.orderPrice }
-        return snapshots
+        val sorted  = snapshots.sortedBy { it.orderPrice }
+//        ObservableSnapshotArray<>(SnapshotParser<> { it.child() })
+        //snapshots
+        return sorted
     }
 
     override fun getItem(position: Int): T {
